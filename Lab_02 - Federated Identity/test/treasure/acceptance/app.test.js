5e836c5ae2c40221f69501c2eaf622790a9fb758
Joel Mussman <jmussman@smallrock.net>
2024-06-06 16:33:46 -0400

    Addtional tests and test updates for Lab 02.

---
+++ Lab_02 - Federated Identity/test/treasure/acceptance/app.test.js
@@ -0,0 +1,35 @@
+// app.test.js
+// Copyright © 2024 Joel A Mussman and NextStep IT Training powered by Smallrock. All rights reserved.
+//
+
+import { beforeAll, describe, expect, it, vi } from 'vitest'
+
+describe('homeController', () => {
+
+    it('Launches automatically from the command line', async () => {
+        
+        process.chdir('./src/treasure')                  // .env is in the pyrates folder; pool is set to 'forks' in vitest.config.js to allow this.
+        delete(process.env.BASE_URL)                    // vitest sets BASE_URL (undocumented) so removing to the the .env setting is the easiest solution.
+
+        // Configure the spy for the homeController.
+
+        const homeController = await import('../../../src/treasure/homeController')
+        const homeControllerSpy = vi.spyOn(homeController, 'default')
+
+        // Setting the argument will cause the application to see execution as starting from the command line.
+
+        process.argv[1] = 'app.js'
+
+        // Importing the module triggers the launch.
+
+        const app = await import('../../../src/treasure/app')
+
+        app.shutdown();
+        
+        // dotenv runs during the launch, so these environment values are from the actual launch.
+
+        expect(homeControllerSpy).toBeCalledWith(process.env.ISSUER_BASE_URL, process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.SECRET, process.env.APPLICATION_PORT, process.env.BASE_URL, process.env.PYRATES_URL)
+    })
+})
+
+export default {}
